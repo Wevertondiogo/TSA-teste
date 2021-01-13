3e59367c4642fa9431ba77a7edfd2e8e919946c9
Weverton procópio <wevertonprocopio690@gmail.com>
2021-01-13 15:37:23 -0300

    função formart refatorada

--- src/scripts/components/format.js
+++ src/scripts/components/format.js
@@ -1,10 +1,35 @@
 const CPF = document.querySelector("#CPF");
+const cardNumber = document.querySelector("#cardNumber");
+//RETORNANDO INDEX E INCREMENT
+const increment = (...increment) => increment[0];
+const handleIndex = (...index) => index[0];
 
-function formatCPF(CPF) {
-  const length = CPF.value.length;
-  if (length === 3) CPF.value += ".";
-  if (length === 7) CPF.value += ".";
-  if (length === 11) CPF.value += "-";
-  return email.value;
+const formatCardNumber = [" ", " ", " "];
+const cardNumberIndex = [4, 9, 14];
+
+const formatCPF = [".", ".", "-"];
+const CPFIndex = [3, 7, 11];
+
+function formatField(field, increment, index) {
+  const length = field.value.length;
+
+  const [i1, i2, i3] = index;
+  const [inc1, inc2, inc3] = increment;
+
+  if (length === i1) field.value += inc1;
+  if (length === i2) field.value += inc2;
+  if (length === i3) field.value += inc3;
+  return field.value;
 }
-CPF.addEventListener("keydown", (event) => formatCPF(event.target));
+
+CPF.addEventListener("keydown", (event) =>
+  formatField(event.target, increment(formatCPF), handleIndex(CPFIndex))
+);
+
+cardNumber.addEventListener("keydown", (event) =>
+  formatField(
+    event.target,
+    increment(formatCardNumber),
+    handleIndex(cardNumberIndex)
+  )
+);
